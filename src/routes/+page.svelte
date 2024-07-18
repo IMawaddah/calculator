968a3362ed11075041b672381e6970802c0a5d38
Mawaddah <mwd.alshebly@gmail.com>
2024-07-18 13:27:29 +0300

    calculator 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </svelte:head>
 
 
-  <div class="bg-white h-fit w-fit rounded-3xl grid grid-cols-4 gap-3 p-5 pt-7 font-semibold shadow-xl text-3xl">
+  <div class="bg-white h-fit w-fit rounded-3xl grid grid-cols-4 gap-3 p-6 pt-7 font-semibold shadow-xl text-3xl">
    <div class="bg-violet-500 rounded-full col-span-4 h-20 flex items-center px-6 mb-2 text-white shadow-xl"> 123456</div>
     <button>%</button>
     <button>&radic;</button>
@@ -14,18 +14,18 @@
     <button>7</button>
     <button>8</button>
     <button>9</button>
-    <button class="text-white bg-violet-600 border-0">-</button>
+    <button class="text-white text-5xl bg-violet-600 border-0">-</button>
     <button>4</button>
     <button>5</button>
     <button>6</button>
-    <button class="text-white bg-violet-700 border-0">÷</button>
+    <button class="text-white text-5xl bg-violet-700 border-0">÷</button>
     <button>1</button>
     <button>2</button>
     <button>3</button>
     <button class="text-white bg-violet-800 border-0">X</button>
-    <button>.</button>
+    <button class="text-4xl">.</button>
     <button>0</button>
     <button class="bg-violet-200 border border-0">=</button>
-    <button class="text-white bg-violet-900 border-0">+</button>
+    <button class="text-white text-5xl bg-violet-900 border-0">+</button>
   </div> 
 
