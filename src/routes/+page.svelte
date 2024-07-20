24a508c6d190112dab0451d3893107ec81c58b53
Mawaddah <mwd.alshebly@gmail.com>
2024-07-20 18:43:59 +0300

    calculator making the buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import PercentIcon from "$lib/Icons/percenticon.svelte";
   import Squarerooticon from "$lib/Icons/Square root icon.svelte";
   import Subtractionicon from "$lib/Icons/subtraction icon.svelte";
@@ -6,7 +6,23 @@
   import Multiplicationicon from "$lib/Icons/multiplication icon.svelte";
   import Equalityicon from "$lib/Icons/equality icon.svelte";
   import Additionicon from "$lib/Icons/addition icon.svelte";
+  let equation: string= "";
+
+  function addToEquation(value: string) {
+    equation+=value;
+  }
+
+  function clear(){
+  equation = " ";
+  }
+  function backspace(){
+    equation = equation.substring(0,equation.length-1).trim().trimStart();
+    
+  
+}
 </script>
+
+
 
 
 <svelte:head>
@@ -15,50 +31,47 @@
   </title>
 </svelte:head>
 
-
-  <div class="bg-white h-fit w-fit rounded-3xl grid grid-cols-4 gap-3 p-6 pt-7 font-semibold shadow-xl text-3xl">
-   <div class="bg-violet-500 rounded-full col-span-4 h-20 flex items-center px-6 mb-2 text-white shadow-xl"> 123456</div>
-    <button class="bg-violet-200 ">
+<!--TODO: fix overflowing numbers-->
+  <div class="bg-white h-fit w-fit rounded-3xl grid grid-cols-4 gap-3 p-6 pt-7 font-semibold shadow-xl text-3xl"> 
+   <div class="bg-violet-500 rounded-full col-span-4 h-20 flex items-center px-6 mb-2 text-white shadow-xl"> {equation}</div>
+    <button on:click={()=>addToEquation(' / 100')} class="bg-violet-200 ">
       <!-- %-->
       <PercentIcon/>
     </button>
-    <button class=" bg-violet-200">
-      <!-- Square root icon-->
-      <Squarerooticon/>
-    </button>
-    <button class="bg-violet-200">CE</button>
-    <button class="text-white bg-violet-500 border-0">C</button>
-    <button>7</button>
-    <button>8</button>
-    <button>9</button>
-    <button class="text-white bg-violet-600 border-0">
+  
+    <button on:click={backspace} class="bg-violet-200">CE</button>
+    <button on:click={clear} class=" bg-violet-200">C</button>
+     <button on:click={()=>addToEquation(' - ')} class="text-white bg-violet-500">
       <!-- subtraction icon-->
       <Subtractionicon/>
     </button>
-    <button>4</button>
-    <button>5</button>
-    <button>6</button>
-    <button class="text-white bg-violet-700">
+    <button on:click={()=>addToEquation('7')}>7</button>
+    <button on:click={()=>addToEquation('8')}>8</button>
+     <button on:click={()=>addToEquation('9')}>9</button>
+     <button on:click={()=>addToEquation(' / ')} class="text-white bg-violet-600">
       <!--division icon -->
       <Divisionicon/>
     </button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button class="text-white bg-violet-800">
+    <button on:click={()=>addToEquation('4')}>4</button>
+    <button on:click={()=>addToEquation('5')}>5</button>
+    <button on:click={()=>addToEquation('6')}>6</button>
+        <button on:click={()=>addToEquation(' * ')} class="text-white bg-violet-700">
       <!-- multiplication icon -->
       <Multiplicationicon/>
     </button>
-    <button class="text-4xl">.</button>
-    <button>0</button>
-    <button class="bg-violet-200 ">
-      <!-- equality icon -->
-       <Equalityicon/>
-    </button>
-    <button class="text-white bg-violet-900 ">
+    <button on:click={()=>addToEquation('1')} >1</button>
+    <button on:click={()=>addToEquation('2')}>2</button>
+    <button on:click={()=>addToEquation('3')} >3</button>
+    <button on:click={()=>addToEquation(' + ')} class="text-white bg-violet-800 ">
       <!-- addition icon-->
       <Additionicon/>
     </button>
-    
+    <button on:click={()=>addToEquation('.')} class="text-4xl">.</button>
+    <button on:click={()=> addToEquation('0')} class="col-span-2 w-full flex items-center px-6 mb-2">0</button>
+    <button on:click={()=>addToEquation(' = ')} class="bg-violet-900 text-white">
+      <!-- equality icon -->
+       <Equalityicon/>
+    </button>
+
   </div> 
 
